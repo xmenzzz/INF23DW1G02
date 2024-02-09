4b346e2d1263b445cfdbfaa4dc993e0fc128d2ed
xmenzzz <dvpximenes@gmail.com>
2024-02-09 17:30:19 +0000

    temporada updated

--- service/TemporadaControllerService.js
+++ service/TemporadaControllerService.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var sql = require('../dbconnect.js');
 
 /**
  * Retrieve Temporada
@@ -8,21 +9,16 @@
  **/
 exports.retrieveTemporada = function() {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "id_temporada" : 0,
-  "nome_temporada" : "nome_temporada",
-  "pontos_acumulados" : 6
-}, {
-  "id_temporada" : 0,
-  "nome_temporada" : "nome_temporada",
-  "pontos_acumulados" : 6
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+    sql.query("SELECT * FROM temporada", function (err, res){
+      if (err) {
+        console.log(err)
+        reject(err);
+      }
+      else {
+        console.log(res);
+        resolve(res[0]);
+      }
+    }) 
   });
 }
 
@@ -35,17 +31,30 @@ exports.retrieveTemporada = function() {
  **/
 exports.retrieveTemporadaId = function(id_temporada) {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "id_temporada" : 0,
-  "nome_temporada" : "nome_temporada",
-  "pontos_acumulados" : 6
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+    sql.query("SELECT * FROM temporada WHERE id = ?", [id_temporada], function (err, res){
+      if (err) {
+        console.log(err)
+        reject(err);
+      }
+      else {
+        console.log(res);
+        resolve(res[0]);
+      }
+    }) 
   });
 }
 
+
+/**
+ * Retrieve Temporada on temporada
+ *
+ * id_temporada Long 
+ * returns Temporada
+ **/
+exports.retrieveTemporadaTemporadaId = function(id_temporada) {
+  return new Promise(function(resolve, reject) {
+    // This function does not make sense in the context of accessing a temporada by temporada ID as it's already done above.
+    // Adjust or remove based on actual use case.
+    resolve("Function implementation may need revision - accessing temporada by temporada ID is already covered in retrieveTemporadaId.");
+  });
+}
