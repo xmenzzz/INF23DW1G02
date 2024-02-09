194d427b492ffa6d21a7d41d9ad3b02b18347d8e
xmenzzz <dvpximenes@gmail.com>
2024-02-09 17:09:55 +0000

    estatisticas updated

--- service/EstatisticasControllerService.js
+++ service/EstatisticasControllerService.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var sql = require('../dbconnect.js');
 
 /**
  * Retrieve Estatistica
@@ -8,50 +9,54 @@
  **/
 exports.retrieveEstatistica = function() {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "golos_sofridos" : 1,
-  "faltas_sofridas" : 5,
-  "id_estatistica" : 0,
-  "golos_marcados" : 6,
-  "faltas_cometidas" : 5
-}, {
-  "golos_sofridos" : 1,
-  "faltas_sofridas" : 5,
-  "id_estatistica" : 0,
-  "golos_marcados" : 6,
-  "faltas_cometidas" : 5
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+    sql.query("SELECT * FROM estatistica", function (err, res) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      else {
+        console.log(res);
+        resolve(res[0]);
+      }
+    }) 
   });
 }
 
-
 /**
- * Retrieve Estatisticas by ID
+ * Retrieve Estatistica by ID
  *
  * id_estatistica Long 
- * returns Estatisticas
+ * returns Estatistica
  **/
 exports.retrieveEstatisticaId = function(id_estatistica) {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "golos_sofridos" : 1,
-  "faltas_sofridas" : 5,
-  "id_estatistica" : 0,
-  "golos_marcados" : 6,
-  "faltas_cometidas" : 5
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+    sql.query("SELECT * FROM estatistica WHERE id = ?", [id_estatistica], function (err, res) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      else {
+        console.log(res);
+        resolve(res[0]);
+      }
+    }) 
   });
 }
 
+/**
+ * Retrieve Estatistica on Temporada
+ *
+ * This functionality would depend on how estatistica is related to temporada.
+ * Assuming there's a direct relation or a way to correlate them through another table.
+ * Here's a placeholder implementation to be adapted based on actual schema.
+ **/
+exports.retrieveEstatisticaTemporadaId = function(id_temporada) {
+  return new Promise(function(resolve, reject) {
+    // Placeholder: The actual query depends on your database schema
+    // For example, if `estatistica` has a `id_temporada` column:
+    // sql.query("SELECT * FROM estatistica WHERE id_temporada = ?", [id_temporada], function (err, res) {
+    
+    // Placeholder response to indicate this needs to be implemented according to actual database schema.
+    resolve("Function implementation depends on database schema relating estatistica to temporada.");
+  });
+}
